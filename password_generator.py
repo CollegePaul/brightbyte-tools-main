ebe4f594dd47cc5f8a34e253d651de980e5888d3
160233749-design <160233749@student.rnngroup.ac.uk>
2026-01-27 19:47:49 +0000

    command line added

--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,7 @@
 import random
 import string
+# command line
+import sys
 
 def generate_password(length):
       # Define characters we want to use
@@ -13,8 +15,13 @@ def generate_password(length):
     
 
 print("--- Welcome to the Simple Password Generator ---")
+# New Features
+if len(sys.argv)<2:
+  
 
 user_length = int(input("How many characters should the password be? "))
+
+
 
 new_password = generate_password(user_length)
 print(f"Your new password is: {new_password}")
